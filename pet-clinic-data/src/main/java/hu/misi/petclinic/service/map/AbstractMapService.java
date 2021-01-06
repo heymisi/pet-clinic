17ae85ba8a18aa3cbbd1beeed593de6c2e800922
HeyMisi <heymisi99@gmail.com>
2021-01-06 21:42:22 +0100

    Refactoring Map impl to abstract Id generation.

--- pet-clinic-data/src/main/java/hu/misi/petclinic/service/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/hu/misi/petclinic/service/map/AbstractMapService.java
@@ -1,12 +1,13 @@
 package hu.misi.petclinic.service.map;
 
-import hu.misi.petclinic.model.BaseEntity;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
-import java.util.*;
+public abstract class AbstractMapService<T, ID> {
 
-public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
-
-    protected Map<Long, T> map = new HashMap<>();
+    protected Map<ID, T> map = new HashMap<>();
 
     Set<T> findAll(){
       return new HashSet<>(map.values());
@@ -16,17 +17,8 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
         return map.get(id);
     }
 
-    T save(T object){
-
-        if(object != null){
-            if(object.getId() == null){
-                object.setId(getNextId());
-            }
-
-            map.put(object.getId(),object);
-        } else{
-            throw new RuntimeException("Object cannot be null");
-        }
+    T save(ID id, T object){
+        map.put(id,object);
 
         return object;
     }
@@ -37,15 +29,5 @@ public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> 
 
     void delete(T object){
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
-    }
-
-    private Long getNextId(){
-        Long nextId = null;
-        try {
-            nextId = Collections.max(map.keySet()) + 1;
-        }catch (NoSuchElementException e){
-            nextId = 1L;
-        }
-        return nextId;
     }
 }
