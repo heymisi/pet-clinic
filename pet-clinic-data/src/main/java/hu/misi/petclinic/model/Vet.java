65e17a478bb630e0f4b52b2f5fef22d30b071b1d
HeyMisi <heymisi99@gmail.com>
2021-01-12 20:26:19 +0100

    Added Project Lombok to project.

--- pet-clinic-data/src/main/java/hu/misi/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/hu/misi/petclinic/model/Vet.java
@@ -1,9 +1,16 @@
 package hu.misi.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -13,11 +20,4 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialties = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialties;
-    }
-
-    public void setSpecialities(Set<Speciality> specialties) {
-        this.specialties = specialties;
-    }
 }
