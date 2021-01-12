078d9ab28be3ccb034ec7e04ec79eb3dd9139798
HeyMisi <heymisi99@gmail.com>
2021-01-12 14:00:40 +0100

    added spring data jpa service for Pet and Specialities.

---
+++ pet-clinic-data/src/main/java/hu/misi/petclinic/services/springdatajpa/SpecialitySDJpaService.java
@@ -0,0 +1,48 @@
+package hu.misi.petclinic.services.springdatajpa;
+
+import hu.misi.petclinic.model.Speciality;
+import hu.misi.petclinic.repositories.SpecialtyRepository;
+import hu.misi.petclinic.services.SpecialityService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class SpecialitySDJpaService implements SpecialityService {
+
+    private final SpecialtyRepository specialityRepository;
+
+    public SpecialitySDJpaService(SpecialityService specialityService, SpecialtyRepository specialityRepostiory, SpecialtyRepository specialityRepository) {
+        this.specialityRepository = specialityRepository;
+    }
+
+    @Override
+    public Set<Speciality> findAll() {
+        Set<Speciality> specialities = new HashSet<>();
+        specialityRepository.findAll().forEach(specialities::add);
+        return specialities;
+    }
+
+    @Override
+    public Speciality findById(Long aLong) {
+        return specialityRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return specialityRepository.save(object);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        specialityRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        specialityRepository.deleteById(aLong);
+    }
+}
