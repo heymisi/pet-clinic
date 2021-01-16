cb5639e7a4b8c7d34030f321c081dff88321f345
HeyMisi <heymisi99@gmail.com>
2021-01-16 23:00:48 +0100

    added test

--- pet-clinic-data/src/main/java/hu/misi/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/hu/misi/petclinic/model/Owner.java
@@ -1,6 +1,9 @@
 package hu.misi.petclinic.model;
 
-import lombok.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -9,8 +12,6 @@ import java.util.Set;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
@@ -26,5 +27,18 @@ public class Owner extends Person {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+
+        if(pets != null) {
+            this.pets = pets;
+        }
+    }
 
 }
--- pet-clinic-data/src/main/java/hu/misi/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/hu/misi/petclinic/model/Person.java
@@ -1,6 +1,8 @@
 package hu.misi.petclinic.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
@@ -8,6 +10,8 @@ import javax.persistence.MappedSuperclass;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
 public class Person extends BaseEntity{
 
@@ -17,4 +21,9 @@ public class Person extends BaseEntity{
     @Column(name = "last_name")
     private String lastName;
 
+    public Person(Long id, String firstName, String lastName) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
